ba9b645ffaa12b479ef9c42bc6165272738c9dda
Mayrani Vargas <mayrani2105@gmail.com>
2020-12-23 08:15:54 -0600

    Creacion del proyecto, Inicio
    Signed-off-by: Mayrani Vargas <mayrani2105@gmail.com>

---
+++ prueba/database/migrations/2020_12_23_141344_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->incremets('id');
+            $table->string('nombre');
+            $table->srting('unidad_medida');
+            $table->double('precio');
+            $table->double('stock');
+            $table->double('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
